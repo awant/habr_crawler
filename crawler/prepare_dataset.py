1f4f1474ef1eba52fa5ee71fb8680a42aa8243a9
Roman Marakulin <marakulin@joom.com>
2020-02-24 14:47:28 +0300

    add code to crawle habr.com

---
+++ crawler/prepare_dataset.py
@@ -0,0 +1,230 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import json
+import re
+from nltk.tokenize import sent_tokenize, word_tokenize
+import statistics
+import string
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from collections import Counter, defaultdict
+from pprint import pprint
+import os
+from tqdm import tqdm
+
+
+def get_meta_info(soup):
+    json_scripts = soup.find_all('script', {'type': 'application/ld+json'})
+    if not json_scripts:
+        return
+    json_script = json.loads(json_scripts[0].text)
+    return {
+        'link': json_script['mainEntityOfPage'].get('@id'),
+        'title': json_script.get('headline'),
+        'published_date': json_script['datePublished'][:10],
+        'published_time': json_script['datePublished'][11:16],
+        'modified_date': json_script['dateModified'][:10],
+        'modified_time': json_script['dateModified'][11:16],
+        'author_type': json_script['author'].get('@type'),
+        'author_name': json_script['author'].get('name'),
+        'description': json_script.get('description'),
+        'image': json_script.get('image', [''])[0],
+        'article_categories': ', '.join(json_script.get('about', []))
+    }
+
+
+def get_votes(soup):
+    labels = soup.find('span', {'class': 'voting-wjt__counter voting-wjt__counter_positive js-score'})
+    if not labels:
+        return 0, 0
+    text = labels['title']
+    m = re.search(r'↑(\d+).*↓([-]?\d+)', text)
+    if len(m.groups()) < 2:
+        return 0, 0
+    return int(m.group(1)), int(m.group(2))
+
+
+def get_bookmarks(soup):
+    label = soup.find('span', {'class': 'bookmark__counter js-favs_count'})
+    if not label:
+        return 0
+    return int(label.text)
+
+
+def get_views(soup):
+    label = soup.find('span', {'class': 'post-stats__views-count'})
+    if not label:
+        return 0
+    text = label.text.replace(',', '.')
+    try:
+        if text[-1] == 'k':
+            text = float(text[:-1]) * 1000
+        return int(text)
+    except ValueError:
+        return 0
+
+
+def get_comments(soup):
+    label = soup.find('span', {'class': 'post-stats__comments-count'})
+    if not label:
+        return 0
+    return int(label.text)
+
+
+def get_target_counters(soup):
+    positive_votes, negative_votes = get_votes(soup)
+
+    return {
+        'positive_votes': positive_votes,
+        'negative_votes': negative_votes,
+        'rating': positive_votes - negative_votes,
+        'bookmarks': get_bookmarks(soup),
+        'views': get_views(soup),
+        'comments': get_comments(soup)
+    }
+
+
+def get_body(soup):
+    soup_body = soup.find('div', {'class': 'post__body post__body_full'})
+    if not soup_body:
+        soup_body = soup.find('div', {'class': 'article__body'})
+    [x.extract() for x in soup_body.findAll('script')]
+    [x.extract() for x in soup_body.findAll('style')]
+    return soup_body.text
+
+
+def get_meta_features(soup):
+    soup_body = soup.find('div', {'class': 'post__body post__body_full'})
+    if not soup_body:
+        soup_body = soup.find('div', {'class': 'article__body'})
+    [x.extract() for x in soup_body.findAll('script')]
+    [x.extract() for x in soup_body.findAll('style')]
+
+    href_count = len(soup_body.find_all('a', href=True))
+    img_count = len([x for x in soup_body.find_all('img')])
+    h3_count = len([x for x in soup_body.find_all('h3')])
+    i_count = len([x for x in soup_body.find_all('i')])
+    spoiler_count = len(soup_body.find_all('div', {'class': 'spoiler'}))
+    tags = soup.find('meta', {'name': 'keywords'})
+    if tags:
+        tags = tags.get('content')
+    else:
+        tags = soup.find_all('li', {'class': 'inline-list__item inline-list__item_tag'})
+        tags = ', '.join([x.text for x in tags])
+
+    return {
+        'href_count': href_count,
+        'img_count': img_count,
+        'tags': tags,
+        'h3_count': h3_count,
+        'i_count': i_count,
+        'spoiler_count': spoiler_count
+    }
+
+
+def get_text_features(soup, language='russian'):
+    text = get_body(soup)
+    lines = list(filter(None, text.split('\n')))
+    joined_lines = ' '.join(lines)
+    sentences = sent_tokenize(joined_lines, language)
+    sent_lens = [len(x) for x in sentences]
+    if not sent_lens:
+        sent_lens = [0]
+
+    tokens = word_tokenize(text, language)
+    tokens_lens = [len(x) for x in tokens]
+    if not tokens_lens:
+        tokens_lens = [0]
+    alphabetic_tokens = [token.lower() for token in tokens if token.isalpha()]
+
+    table = str.maketrans('', '', string.punctuation)
+    stripped_atokens = [w.translate(table) for w in alphabetic_tokens]
+
+    stop_words = set(stopwords.words(language))
+    words = [tkn for tkn in stripped_atokens if tkn not in stop_words]
+
+    most_common_words = [x[0] for x in Counter(words).most_common(10)]
+
+    stemmer = SnowballStemmer(language)
+    words = [stemmer.stem(word) for word in words]
+    words_len = [len(x) for x in words]
+    if not words_len:
+        words_len = [0]
+
+    return {
+        'text_len': len(text),
+        'lines_count': len(lines),
+        'sentences_count': len(sentences),
+        'first_5_sentences': ' '.join(sentences[:5]),
+        'last_5_sentences': ' '.join(sentences[-5:]),
+        'max_sentence_len': max(sent_lens),
+        'min_sentence_len': min(sent_lens),
+        'mean_sentence_len': statistics.mean(sent_lens),
+        'median_sentence_len': statistics.median(sent_lens),
+        'tokens_count': len(tokens),
+        'max_token_len': max(tokens_lens),
+        'mean_token_len': statistics.mean(tokens_lens),
+        'median_token_len': statistics.median(tokens_lens),
+        'alphabetic_tokens_count': len(alphabetic_tokens),
+        'words_count': len(words),
+        'words_mean': statistics.mean(words_len),
+        'ten_most_common_words': ', '.join(most_common_words)
+    }, text
+
+
+def parse_html(filepath):
+    soup = BeautifulSoup(open(filepath, 'r'), 'html.parser')
+
+    meta = get_meta_info(soup)
+    meta_features = get_meta_features(soup)
+    counters = get_target_counters(soup)
+    text_features, text = get_text_features(soup)
+
+    features = {**meta, **meta_features, **counters, **text_features}
+    jsonified_text_raw = {'text': text, 'link': features['link']}
+
+    return features, jsonified_text_raw
+
+
+def build_dataset(filepath='data/pages', out_df='data/data.csv', out_text='data/texts.json'):
+    data = []
+    with open(out_text, 'w', encoding="utf-8") as f:
+        for (root, dirs, files) in tqdm(os.walk(filepath)):
+            for file in files:
+                fp = os.path.join(root, file)
+                features, jsonified_text_raw = parse_html(fp)
+                if features['sentences_count'] > 0 and features['link'] and features['negative_votes'] >= 0:
+                    data.append(features)
+                    f.write(json.dumps(jsonified_text_raw, ensure_ascii=False)+'\n')
+
+    df = pd.DataFrame(data)
+    df.to_csv(out_df, index=False)
+
+
+def print_stats(filepath='data/pages'):
+    s = 0
+    data = defaultdict(int)
+    for (root, dirs, files) in os.walk(filepath):
+        date = root[-10:-6]
+        if date.startswith(filepath[:4]):
+            continue
+        data[date] += len(files)
+        s += len(files)
+    for date, cnt in sorted(list(data.items()))[::-1]:
+        print(date, cnt)
+    print(f'sum = {s}')
+
+
+def test():
+    feat, text = parse_html('data/pages/2008-07-31/63806050')
+    pprint(feat)
+
+
+def main():
+    # test()
+    # print_stats()
+    build_dataset()
+
+
+if __name__ == '__main__':
+    main()
---
+++ crawler/scraper.py
@@ -0,0 +1,139 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+from tqdm import tqdm
+import json
+import os
+import pickle
+
+
+class HabrScraper(object):
+    habr_url = 'https://habr.com/ru/all/all'
+    paging_csv_fp = 'data/paging.csv'
+    pages_folder = 'data/pages'
+    cache_fp = 'data/cache.pickle'
+
+    @staticmethod
+    def download_page(url, throttling=1):
+        time.sleep(throttling)
+        response = requests.get(url)
+        return response.text
+
+    @staticmethod
+    def dump_page_tmp(url, file='dump.html'):
+        response = requests.get(url)
+        with open(file, mode='w') as f:
+            f.write(response.text)
+
+    @staticmethod
+    def extract_pages_links(html):
+        soup = BeautifulSoup(html, 'html.parser')
+        return [{'link': link['href'], 'title': link.text} for link in soup.find_all('a', {'class': 'post__title_link'})]
+
+    @staticmethod
+    def parse_paging():
+        pages = []
+        paging_urls = ['{}/page{}'.format(HabrScraper.habr_url, page_idx) for page_idx in range(1, 50+1)]
+        for url in tqdm(paging_urls):
+            html = HabrScraper.download_page(url)
+            pages += HabrScraper.extract_pages_links(html)
+        df = pd.DataFrame(pages)
+        df.to_csv(HabrScraper.paging_csv_fp, index=False)
+
+    @staticmethod
+    def is_article(soup):
+        return len(soup.find_all('meta', {'property': 'og:type', 'content': 'article'})) > 0
+
+    @staticmethod
+    def get_date_str(soup):
+        json_scripts = soup.find_all('script', {'type': 'application/ld+json'})
+        if not json_scripts:
+            return None
+        json_script = json.loads(json_scripts[0].text)
+        date_str = json_script.get('datePublished', '')[:10]
+        if len(date_str) < 10:
+            return None
+        return date_str
+
+    @staticmethod
+    def get_article_id(url):
+        return abs(hash(url)) % (10 ** 8)
+
+    @staticmethod
+    def dump_page(html, fp, folder):
+        if not os.path.exists(fp):
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            with open(fp, mode='w') as f:
+                f.write(html)
+
+    @staticmethod
+    def dump_page_if_article(url, soup, html):
+        if HabrScraper.is_article(soup):
+            date = HabrScraper.get_date_str(soup)
+            if date:
+                idx = str(HabrScraper.get_article_id(url))
+                fp = os.path.join(HabrScraper.pages_folder, date, idx)
+                folder = os.path.join(HabrScraper.pages_folder, date)
+                HabrScraper.dump_page(html, fp, folder)
+
+    @staticmethod
+    def extract_links(url):
+        html = HabrScraper.download_page(url)
+        soup = BeautifulSoup(html, 'html.parser')
+        HabrScraper.dump_page_if_article(url, soup, html)
+        extracted_links = [link.get('href') for link in soup.find_all('a')]
+        extracted_links = [x for x in extracted_links if x and ('#' not in x)]
+        return list(set([link for link in extracted_links if link.startswith('https://habr.com/ru/') and ('users' not in link)]))
+
+    @staticmethod
+    def find_all_pages_links_recursively(link, known_links):
+        new_links = [x for x in HabrScraper.extract_links(link) if x not in known_links]
+        known_links.update(new_links)
+        return new_links, known_links
+
+    @staticmethod
+    def init_caches():
+        if os.path.exists(HabrScraper.cache_fp):
+            with open(HabrScraper.cache_fp, 'rb') as f:
+                cache = pickle.load(f)
+                links, known_links = cache['links'], cache['known_links']
+                links = [x for x in links if x and ('#' not in x) and ('users' not in x)]
+                post_links = [x for x in links if '/post/' in x]
+                other_links = [x for x in links if '/post/' not in x]
+                links = post_links + other_links
+            return links, known_links
+        else:
+            links = list(pd.read_csv(HabrScraper.paging_csv_fp)['link'])
+            known_links = set(links)
+            return links, known_links
+
+    @staticmethod
+    def dump_cache(links, known_links):
+        cache = {'links': links, 'known_links': known_links}
+        with open(HabrScraper.cache_fp, 'wb') as f:
+            pickle.dump(cache, f)
+
+    @staticmethod
+    def dump_pages():
+        links, known_links = HabrScraper.init_caches()
+        iterations = 0
+        while len(links) > 0:
+            new_links, known_links = HabrScraper.find_all_pages_links_recursively(links[0], known_links)
+            links_set = set(links)
+            new_links = [x for x in new_links if x not in links_set]
+            links = links[1:] + new_links
+            iterations += 1
+            if iterations % 10 == 0:
+                HabrScraper.dump_cache(links, known_links)
+            print('iter: {}, links_size: {}, set_size: {}'.format(iterations, len(links), len(known_links)))
+
+
+def main():
+    HabrScraper.parse_paging()
+    HabrScraper.dump_pages()
+
+
+if __name__ == '__main__':
+    main()
